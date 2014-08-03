13d65ea4baf9ffd8c4928a2b6055abf740197588
Lucas DeRego <lucas.derego@gmail.com>
2014-08-02 20:27:26 -1000

    finally done cleaning up

--- src/net.py
+++ src/net.py
@@ -59,7 +59,7 @@ class Client:
         self.handlers = []
     
     def send(self, packet):
-        self.peer.send(0, enet.Packet(packet.to_bytes()))
+        self.peer.send(0, enet.Packet(packet.to_bytes(), enet.PACKET_FLAG_RELIABLE))
 
     def add_handler(self, handler):
         self.handlers.append(handler)
@@ -80,7 +80,7 @@ class Client:
             elif event.type == enet.EVENT_TYPE_RECEIVE:
                 packets.append(Packet(event.packet.data))
                 for handler in self.handlers:
-                    handler.handle_packet(Packet(event.packet.data))
+                    handler.handle_packet(Packet(event.packet.data), 0)
         return packets
 
 
@@ -89,16 +89,16 @@ class Client:
 class Server:
     
     def __init__(self, port):
-        self.host = enet.Host(enet.Address(b"localhost", port), 10, 0, 0, 0)
+        self.host = enet.Host(enet.Address(None, port), 10, 0, 0, 0)
         self.peers = {} # Map client IDs to peers
         self.handlers = []
         self.next_client_id = 1
 
     def broadcast(self, packet):
-        self.host.broadcast(0, enet.Packet(packet.to_bytes()))
+        self.host.broadcast(0, enet.Packet(packet.to_bytes(), enet.PACKET_FLAG_RELIABLE))
 
     def send(self, client_id, packet):
-        self.peers[client_id].send(0, enet.Packet(packet.to_bytes()))
+        self.peers[client_id].send(0, enet.Packet(packet.to_bytes(), enet.PACKET_FLAG_RELIABLE))
 
     def add_handler(self, handler):
         self.handlers.append(handler)
@@ -114,7 +114,7 @@ class Server:
             if event.type == enet.EVENT_TYPE_CONNECT:
                 print("Client "+str(client_id)+" connected from " + str(event.peer.address))
                 # Send client ID
-                event.peer.send(0, enet.Packet(pickle.dumps(client_id)))
+                event.peer.send(0, enet.Packet(pickle.dumps(client_id), enet.PACKET_FLAG_RELIABLE))
                 # Handle connection
                 self.peers[client_id] = event.peer
                 for handler in self.handlers:
